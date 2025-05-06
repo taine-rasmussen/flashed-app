d8bb13676418db320249ddb54165d708bad98fe2
Taine Rasmussen <taine.rasmussen@gmail.com>
2025-05-06 13:21:51 +0100

    test pre-commit hook

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+import js from '@eslint/js';
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import react from 'eslint-plugin-react';
+import reactNative from 'eslint-plugin-react-native';
+import importPlugin from 'eslint-plugin-import';
+import prettier from 'eslint-config-prettier';
+
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      '@typescript-eslint': ts,
+      react,
+      'react-native': reactNative,
+      import: importPlugin,
+    },
+    rules: {
+      semi: ['error', 'always'],
+      'comma-dangle': ['error', 'always-multiline'],
+      '@typescript-eslint/no-unused-vars': ['error'],
+      'import/order': [
+        'error',
+        {
+          groups: [
+            ['builtin', 'external'],
+            ['internal', 'parent', 'sibling', 'index'],
+          ],
+          'newlines-between': 'always',
+        },
+      ],
+      'react/react-in-jsx-scope': 'off',
+      'react-native/no-inline-styles': 'warn',
+    },
+  },
+  prettier,
+];

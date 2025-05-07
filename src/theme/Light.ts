76a63bf926f0b346b94dfeab80692d09c4a6728a
Taine Rasmussen <taine.rasmussen@gmail.com>
2025-05-07 15:08:11 +0100

    ensure we bundle sf pro fonts to andriod

--- src/theme/Light.ts
+++ src/theme/Light.ts
@@ -1,3 +1,4 @@
+import { Platform } from 'react-native';
 import { MD3LightTheme as DefaultLightTheme } from 'react-native-paper';
 
 import { spacing, typography } from './Util';
@@ -15,5 +16,29 @@ export const lightTheme = {
     surface: '#f9f9f9',
     text: '#1c1c1e',
     border: '#d1d1d6',
+  },
+  fonts: {
+    ...DefaultLightTheme.fonts,
+    bodyLarge: {
+      ...DefaultLightTheme.fonts.bodyLarge,
+      fontFamily: Platform.select({
+        ios: undefined, // IOS default is SF Pro
+        android: 'SF-Pro',
+      }),
+    },
+    titleLarge: {
+      ...DefaultLightTheme.fonts.titleLarge,
+      fontFamily: Platform.select({
+        ios: undefined,
+        android: 'SF-Pro',
+      }),
+    },
+    labelMedium: {
+      ...DefaultLightTheme.fonts.labelMedium,
+      fontFamily: Platform.select({
+        ios: undefined,
+        android: 'SF-Pro',
+      }),
+    },
   },
 };

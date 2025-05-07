79d152419e35eafb3680e9ad78bf6c5c73eef5ab
Taine Rasmussen <taine.rasmussen@gmail.com>
2025-05-07 14:58:24 +0100

    add spacing and typography to theme

---
+++ src/theme/Typography.ts
@@ -0,0 +1,22 @@
+export const typography = {
+  largeTitle: {
+    fontSize: 34,
+    fontWeight: '700',
+  },
+  title1: {
+    fontSize: 28,
+    fontWeight: '600',
+  },
+  title2: {
+    fontSize: 22,
+    fontWeight: '600',
+  },
+  body: {
+    fontSize: 17,
+    fontWeight: '400',
+  },
+  caption: {
+    fontSize: 13,
+    color: '#6e6e6e',
+  },
+};
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,1 +1,3 @@
 export * from './ThemeContext';
+export * from './Spacing';
+export * from './Typography';

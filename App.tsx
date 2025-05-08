b112f752122a6c7424acc7abac430dc6c8f62c0b
Taine Rasmussen <taine.rasmussen@gmail.com>
2025-05-08 10:19:46 +0100

    placeholder splash

--- App.tsx
+++ App.tsx
@@ -1,1 +1,9 @@
+import * as SplashScreen from 'expo-splash-screen';
 export { default } from 'expo-router';
+
+SplashScreen.preventAutoHideAsync();
+
+SplashScreen.setOptions({
+  duration: 3000,
+  fade: true,
+});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,6 @@
+import { useCallback } from 'react';
+import { View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 import { Slot } from 'expo-router';
 import { PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -6,12 +9,26 @@ import { ThemeProvider, useThemeContext } from '@/theme';
 
 export default function RootLayout() {
   const { theme } = useThemeContext();
+  const appReady = true;
+
+  const onLayoutRootView = useCallback(() => {
+    if (appReady) {
+      //  use rm -rf ios to reset cached bg colour on splash image
+      SplashScreen.hide();
+    }
+  }, [appReady]);
+
+  if (!appReady) {
+    return null;
+  }
 
   return (
     <ThemeProvider>
       <SafeAreaProvider>
         <PaperProvider theme={theme}>
-          <Slot />
+          <View onLayout={onLayoutRootView} style={{ flex: 1 }}>
+            <Slot />
+          </View>
         </PaperProvider>
       </SafeAreaProvider>
     </ThemeProvider>

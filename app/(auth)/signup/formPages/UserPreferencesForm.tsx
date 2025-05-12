9ae1fe835975956e6a45b09e69cfafbc6656982b
Taine Rasmussen <taine.rasmussen@gmail.com>
2025-05-12 19:33:13 +0100

    can select a gradestyle!

--- app/(auth)/signup/formPages/UserPreferencesForm.tsx
+++ app/(auth)/signup/formPages/UserPreferencesForm.tsx
@@ -1,8 +1,12 @@
 import { View } from 'react-native';
+import { SegmentedButtons } from 'react-native-paper';
 
 import AppInput from '@/components/AppInput';
+import { useSignup } from '@/contexts/SignupContext';
 
 const UserPreferencesForm = () => {
+  const { form, updateForm } = useSignup();
+
   return (
     <View>
       <AppInput
@@ -17,11 +21,19 @@ const UserPreferencesForm = () => {
         //  value={email}
         //  onChangeText={setEmail}
       />
-      <AppInput
-        label="Grade Style"
-        // placeholder="Password..."
-        //  value={email}
-        //  onChangeText={setEmail}
+      <SegmentedButtons
+        value={form.gradeStyle}
+        onValueChange={e => updateForm({ gradeStyle: e })}
+        buttons={[
+          {
+            value: 'VScale',
+            label: 'V Scale',
+          },
+          {
+            value: 'Font',
+            label: 'Font',
+          },
+        ]}
       />
     </View>
   );

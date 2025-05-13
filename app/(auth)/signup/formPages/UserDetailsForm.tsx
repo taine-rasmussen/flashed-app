e2f891fc91bd389818017300306253f3b9776aa4
Taine Rasmussen <taine.rasmussen@gmail.com>
2025-05-13 12:23:08 +0100

    ui design is hard

--- app/(auth)/signup/formPages/UserDetailsForm.tsx
+++ app/(auth)/signup/formPages/UserDetailsForm.tsx
@@ -1,50 +1,73 @@
 import { View, StyleSheet } from 'react-native';
 import { Text } from 'react-native-paper';
+import { MaterialIcons } from '@expo/vector-icons';
 
 import AppInput from '@/components/AppInput';
 import { useSignup } from '@/contexts/SignupContext';
-import CardWrapper from '@/components/CardWrapper';
+import { useAppTheme } from '@/theme';
 
 const UserDetailsForm = () => {
   const { form, updateForm } = useSignup();
+  const theme = useAppTheme();
+  const styles = getStyles(theme);
 
   return (
-    <View style={{ display: 'flex', gap: 16 }}>
-      <CardWrapper mode="elevated">
-        <Text style={styles.header} variant="displayLarge">
-          Welcome To Flashed
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Text variant="displaySmall">Welcome to Flashed</Text>
+        <Text variant="titleMedium">
+          Lets get started with some basic info to create your climbing profile.
         </Text>
-      </CardWrapper>
-      <CardWrapper>
+      </View>
+      <View style={styles.formContainer}>
         <AppInput
-          label="First Name"
-          placeholder="First name..."
+          leftIcon={<MaterialIcons name="person" size={20} color="#888" />}
+          labelAbove="First Name"
+          placeholder="Enter your first name"
           value={form.firstName}
           onChangeText={e => updateForm({ firstName: e })}
         />
         <AppInput
-          label="Last Name"
-          placeholder="Last name..."
+          leftIcon={<MaterialIcons name="person" size={20} color="#888" />}
+          labelAbove="Last Name"
+          placeholder="Enter your last name"
           value={form.lastName}
           onChangeText={e => updateForm({ lastName: e })}
         />
         <AppInput
-          label="Email"
-          placeholder="you@example.com"
+          labelAbove="Email"
+          placeholder="Enter your email address"
           keyboardType="email-address"
           autoCapitalize="none"
           value={form.email}
           onChangeText={e => updateForm({ email: e })}
+          leftIcon={<MaterialIcons name="email" size={20} color="#888" />}
         />
-      </CardWrapper>
+      </View>
+      <View>
+        <Text style={styles.footerText}>Already have an account? Login</Text>
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  header: {
-    textAlign: 'center',
-  },
-});
+const getStyles = (theme: any) =>
+  StyleSheet.create({
+    container: {
+      display: 'flex',
+      gap: theme.custom.spacing.lg,
+    },
+    headerContainer: {
+      display: 'flex',
+      gap: theme.custom.spacing.md,
+    },
+    formContainer: {
+      display: 'flex',
+      gap: theme.custom.spacing.lmd,
+    },
+    footerText: {
+      textAlign: 'center',
+    },
+  });
 
 export default UserDetailsForm;

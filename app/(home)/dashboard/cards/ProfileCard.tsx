e3b090ff9e06b1264090b41f0a8e6bf61056af6d
Taine Rasmussen <taine.rasmussen@gmail.com>
2025-06-07 13:10:25 +0200

    basic ui for profile card

--- app/(home)/dashboard/cards/ProfileCard.tsx
+++ app/(home)/dashboard/cards/ProfileCard.tsx
@@ -1,22 +1,81 @@
-import { Card } from 'react-native-paper';
-import { StyleSheet } from 'react-native';
+import { Card, Divider, Text } from 'react-native-paper';
+import { StyleSheet, View, Image } from 'react-native';
+import EvilIcons from '@expo/vector-icons/EvilIcons';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 import { useUser } from '@/contexts/UserContext';
+import { AppTheme } from '@/theme/types';
+import { useAppTheme } from '@/theme';
+
 const ProfileCard = () => {
   const { user } = useUser();
+  const theme = useAppTheme();
+  const styles = getStyles(theme);
 
   const fullName = `${user.first_name} ${user.last_name}`;
+  const location = `${user.home_gym}, ${user.location}`;
+
   return (
     <Card elevation={5} style={styles.container}>
-      <Card.Title title={fullName} />
+      <View style={styles.iconWrapper}>
+        <Ionicons
+          name="person-circle"
+          size={32}
+          color={theme.colors.secondary}
+          style={styles.icon}
+        />
+      </View>
+      <Card.Content style={styles.contentWrapper}>
+        <View style={styles.userWrapper}>
+          <Image style={styles.profileImage} source={require('assets/ProfileImage.png')} />
+          <Text variant="headlineMedium">{fullName}</Text>
+        </View>
+        <Divider bold />
+        <View style={styles.location}>
+          <EvilIcons name="location" size={32} color={theme.colors.secondary} />
+          <Text variant="headlineSmall">{location}</Text>
+        </View>
+      </Card.Content>
     </Card>
   );
 };
 
 export default ProfileCard;
 
-const styles = StyleSheet.create({
-  container: {
-    width: '90%',
-  },
-});
+const getStyles = (theme: AppTheme) =>
+  StyleSheet.create({
+    container: {
+      backgroundColor: theme.colors.background,
+      width: '100%',
+      padding: theme.custom.spacing.sm,
+      position: 'relative',
+    },
+    iconWrapper: {
+      position: 'absolute',
+      top: theme.custom.spacing.sm,
+      right: theme.custom.spacing.sm,
+      zIndex: 1,
+    },
+    icon: {},
+    contentWrapper: {
+      display: 'flex',
+      flexDirection: 'column',
+      gap: theme.custom.spacing.sm,
+    },
+    location: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    userWrapper: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      gap: theme.custom.spacing.sm,
+    },
+    profileImage: {
+      height: 70,
+      width: 70,
+      borderRadius: 50,
+    },
+  });
--- app/(home)/dashboard/index.tsx
+++ app/(home)/dashboard/index.tsx
@@ -5,28 +5,31 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import ProfileCard from './cards/ProfileCard';
 
 import { useAppTheme } from '@/theme';
-import { useUser } from '@/contexts/UserContext';
+import { AppTheme } from '@/theme/types';
 
 export default function DashboardScreen() {
-  const { colors } = useAppTheme();
-  const { user } = useUser();
-  console.log(user);
+  const theme = useAppTheme();
+  const styles = getStyles(theme);
 
   return (
-    <SafeAreaView style={[styles.container, { backgroundColor: colors.background }]}>
+    <SafeAreaView style={styles.container}>
       <ProfileCard />
-      <Text style={[styles.text, { color: colors.text }]}>Dashboard</Text>
+      <Text style={[styles.text, { color: theme.colors.text }]}>Dashboard</Text>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 22,
-    fontWeight: '500',
-  },
-});
+const getStyles = (theme: AppTheme) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      width: '100%',
+      padding: theme.custom.spacing.sm,
+      backgroundColor: theme.colors.secondary,
+    },
+    text: {
+      fontSize: 22,
+      fontWeight: '500',
+    },
+  });
